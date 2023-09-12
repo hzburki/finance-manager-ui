35ba57f70f06dbfca29e14a875d108a6f78d888f
Haseeb Burki <hzburki.hzb@gmail.com>
2023-09-12 22:41:26 +0500

    shell created - refactor needed

--- src/screens/transactions.screen.tsx
+++ src/screens/transactions.screen.tsx
@@ -1,26 +1,5 @@
 import React from "react";
 
 export default function TransactionScreen() {
-  return (
-    <div>
-      <div className="h-60 w-52 flex bg-white m-1 justify-center items-center shadow">
-        Box
-      </div>
-      <div className="h-60 w-52 flex bg-white m-1 justify-center items-center shadow">
-        Box
-      </div>
-      <div className="h-60 w-52 flex bg-white m-1 justify-center items-center shadow">
-        Box
-      </div>
-      <div className="h-60 w-52 flex bg-white m-1 justify-center items-center shadow">
-        Box
-      </div>
-      <div className="h-60 w-52 flex bg-white m-1 justify-center items-center shadow">
-        Box
-      </div>
-      <div className="h-60 w-52 flex bg-white m-1 justify-center items-center shadow">
-        Box
-      </div>
-    </div>
-  );
+  return <div className="p-4">Transactions</div>;
 }
